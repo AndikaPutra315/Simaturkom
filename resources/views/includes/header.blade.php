c52f6e42bc9699822e6fe1cebfaa4093a148a74e
AndikaPutra315 <ajidwiandika567@gmai.com>
2025-10-07 09:12:59 +0800

    header u

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -90,9 +90,8 @@
             <li><a href="{{ url('/') }}" class="{{ Request::is('/') ? 'active' : '' }}">Home</a></li>
             <li><a href="{{ url('/hotspot') }}" class="{{ Request::is('hotspot') ? 'active' : '' }}">Hotspot</a></li>
             <li><a href="{{ route('regulasi') }}" class="{{ Request::routeIs('regulasi') ? 'active' : '' }}">Regulasi</a></li>
-            <li><a href="#" class="{{ Request::is('peta-tower') ? 'active' : '' }}">Peta Tower</a></li>
             <li><a href="{{ url('/data-menara') }}" class="{{ Request::is('data-menara') ? 'active' : '' }}">Data Menara</a></li>
-            <li><a href="#" class="{{ Request::is('peta-zona') ? 'active' : '' }}">Peta Zona</a></li>
+            <li><a href="#" class="{{ Request::is('peta-zona') ? 'active' : '' }}">Peta</a></li>
             <li><a href="{{ url('/#data-infrastruktur') }}" class="{{ Request::is('statistik') ? 'active' : '' }}">Statistik</a></li>
         </ul>
     </nav>
